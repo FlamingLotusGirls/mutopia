381cb30141afc0b2bb62dbdbaee0cd9fad3584e9
Eric Gauderman <pixelprizm@gmail.com>
2023-08-19 19:38:33 -0700

    Remove strip struct and just use Adafruit_NeoPixel directly

--- node/node3/node3.ino
+++ node/node3/node3.ino
@@ -13,12 +13,6 @@ const char *password = "flgflgflg";
 #define UNIV_PERIOD 10
 #define PIXELS_PER_UNIV 170
 
-struct Strip
-{
-  Adafruit_NeoPixel pixels;
-  Strip(int len, int pin, int flags) : pixels(len, pin, flags) {}
-};
-
 #define PIN1 4
 #define PIN2 5
 #define PIN3 6
@@ -28,15 +22,15 @@ struct Strip
 #define PIN7 7
 #define PIN8 44
 
-Strip strips[] = {
-    Strip(STRIP_LENGTH, PIN1, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN2, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN3, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN4, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN5, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN6, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN7, NEO_RGB + NEO_KHZ800),
-    Strip(STRIP_LENGTH, PIN8, NEO_RGB + NEO_KHZ800)};
+Adafruit_NeoPixel strips[] = {
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN1, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN2, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN3, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN4, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN5, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN6, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN7, NEO_RGB + NEO_KHZ800),
+    Adafruit_NeoPixel(STRIP_LENGTH, PIN8, NEO_RGB + NEO_KHZ800)};
 
 ArtnetWifi artnet;
 
@@ -69,7 +63,7 @@ void setup()
   Serial.println("FLG Mutopia LED Node");
   for (int i = 0; i < STRIP_COUNT; i++)
   {
-    strips[i].pixels.begin();
+    strips[i].begin();
   }
 
   Serial.print("setup, on core ");
@@ -131,21 +125,21 @@ void loopCore0()
     {
       for (int i = 0; i < STRIP_COUNT; i++)
       {
-        strips[i].pixels.fill(0xE8689E);
-        strips[i].pixels.show();
+        strips[i].fill(0xE8689E);
+        strips[i].show();
       }
       delay(100);
       for (int i = 0; i < STRIP_COUNT; i++)
       {
-        strips[i].pixels.fill(0x35081B);
-        strips[i].pixels.show();
+        strips[i].fill(0x35081B);
+        strips[i].show();
       }
       delay(100);
     }
     for (int i = 0; i < STRIP_COUNT; i++)
     {
-      strips[i].pixels.fill(0x81380E);
-      strips[i].pixels.show();
+      strips[i].fill(0x81380E);
+      strips[i].show();
     }
     delay(100);
 
@@ -167,8 +161,8 @@ void loop()
   case INIT:
     for (int i = 0; i < STRIP_COUNT; i++)
     {
-      strips[i].pixels.fill(INIT_COLORS[currentInitColor]);
-      strips[i].pixels.show();
+      strips[i].fill(INIT_COLORS[currentInitColor]);
+      strips[i].show();
     }
 
     delay(500);
@@ -179,8 +173,8 @@ void loop()
       // Navigate to TRY_WIFI state
       for (int i = 0; i < STRIP_COUNT; i++)
       {
-        strips[i].pixels.fill(INIT_COLORS[4]);
-        strips[i].pixels.show();
+        strips[i].fill(INIT_COLORS[4]);
+        strips[i].show();
       }
       state = TRY_WIFI;
     }
@@ -189,7 +183,7 @@ void loop()
   case RECEIVE_ARTNET:
     for (int i = 0; i < STRIP_COUNT; i++)
     {
-      strips[i].pixels.show();
+      strips[i].show();
     }
 
     break;
@@ -237,13 +231,11 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *d
     return;
   }
 
-  Strip &strip = strips[stripIndex];
-
   // STORE ARTNET DATA INTO PIXEL COLORS
   // Sometimes the received length isn't a multiple of 3 (e.g. Chromatik seems to always send an
   // even length and pad the buffer with a zero if pixel count is odd).
   int numPixelsReceivedThisFrame = length / 3;
 
   int numPixelsToCopySafely = min(STRIP_LENGTH - startPixel, numPixelsReceivedThisFrame);
-  memcpy(strip.pixels.getPixels() + (startPixel * 3), data, numPixelsToCopySafely * 3);
+  memcpy(strips[stripIndex].getPixels() + (startPixel * 3), data, numPixelsToCopySafely * 3);
 }
